9ba37d1e2f84bc05da74d1fb17a87731e55d3436
Tynchtyk Kyias Uulu <tynchtyk642@gmail.com>
2021-11-11 17:52:19 +0600

    terraform network

---
+++ main.tf
@@ -0,0 +1,92 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+
+  credentials = file(var.credentials_file)
+}
+
+module "network" {
+  source = "./network"
+
+  delete_default_routes_on_create = true
+
+
+  #==========================SUBNETS=============================
+  subnets = [
+    {
+      subnet_name     = "presentation-subnet"
+      subnet_ip_range = var.presentation_ip_range
+      subnet_region   = "us-central1"
+    },
+    {
+      subnet_name           = "application-subnet"
+      subnet_ip_range       = var.application_ip_range
+      subnet_region         = "us-central1"
+      subnet_private_access = true
+    },
+    {
+      subnet_name           = "database-subnet"
+      subnet_ip_range       = var.database_ip_range
+      subnet_region         = "us-central1"
+      subnet_private_access = true
+    },
+
+  ]
+
+  #============================ROUTES=============================
+
+  routes = [
+    {
+      name              = "igw-route"
+      destination_range = var.igw_destination
+      tags              = "public"
+      next_hop_internet = "true"
+    }
+  ]
+
+  #=========================FIREWALL-RULES========================
+  firewall_rules = [
+    {
+      name        = "presentation-firewall-rule"
+      direction   = "INGRESS"
+      ranges      = var.presentation_firewall_ranges
+      target_tags = ["public"]
+      source_tags = null
+
+      allow = [{
+        protocol = "all"
+        ports    = null
+      }]
+      deny = []
+    },
+    {
+      name        = "application-firewall-rule"
+      direction   = "INGRESS"
+      ranges      = var.application_firewall_ranges
+      target_tags = ["application"]
+      source_tags = null
+
+      allow = [{
+        protocol = "all"
+        ports    = null
+      }]
+      deny = []
+
+    },
+    {
+      name        = "database-firewall-rule"
+      direction   = "INGRESS"
+      ranges      = var.database_firewall_ranges
+      source_tags = null
+      target_tags = ["database"]
+
+      allow = [{
+        protocol = "all"
+        ports    = null
+      }]
+      deny = []
+    },
+  ]
+}
+
